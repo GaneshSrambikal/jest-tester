dd5736aaa5020cebd33d15345077775da4f05114
unknown <ganesh.srambikal@gmail.com>
2019-02-12 23:02:14 +0530

    excepted

--- sum.test.js
+++ sum.test.js
@@ -4,8 +4,9 @@ test('add 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('object assignment',()=>{
-    const data = {one:1};
-    data['two']=2;
-    expect(data).toEqual({one:1,two:2});
+test('object assignment', () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    data['three'] = 3;
+    expect(data).toEqual({ one: 1, two: 2, three: 3 });
 });
